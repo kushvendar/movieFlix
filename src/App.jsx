a24fa801aca7002ee6e3972f836a39b57e5ae584
Kush <jangidkush1234@gmail.com>
2025-06-20 17:19:34 +0530

    trending section completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Search from './components/Search'
 import Loader from './components/Loader'
 import Moviecard from './components/Moviecard'
 import { useDebounce } from 'react-use'
-import { updateSearchCount } from './appwrite'
+import { updateSearchCount, getTrendingMovie } from './appwrite'
+
+
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
@@ -26,11 +28,12 @@ const App = () => {
   const [movieList,setMovieList] = useState([])
   const [isLoading,setLoading] = useState(false)
   const [debounceInput, setDebounceInput] = useState('')
+  const [trendingList, setTrendingList] = useState([])
 
   // search optimization is required to prevent server crash
   // avoid hiting rate limiting
 
-  useDebounce(()=> setDebounceInput(searchTerm),500,[searchTerm])
+  useDebounce(()=> setDebounceInput(searchTerm),600,[searchTerm])
 
   const fetchMovie = async (query='') => {
     setLoading(true)
@@ -43,7 +46,7 @@ const App = () => {
       }
 
       const data = await response.json()
-      console.log(data)
+      // console.log(data)
 
       if(data.response===false){
         setErrorMessage(data.Error || 'Failed to fetch movies')
@@ -52,6 +55,8 @@ const App = () => {
       }
       setMovieList(data.results || [] )
 
+      updateSearchCount(query,data.results[0])
+
     } catch (error) {
       console.log(`error code : ${error}`)
         setErrorMessage('Error fetching movies. Please try again later')
@@ -59,23 +64,44 @@ const App = () => {
       setLoading(false)
     }
   } 
+  const fetchTrendingList = async () => {
+      try {
+        const data = await getTrendingMovie()
+        setTrendingList(data)
+      } catch (error) {
+        console.log(`Error fetching trending movies ${error}`)
+      }
+    }
 
   useEffect(()=>{
     fetchMovie(debounceInput)
   },[debounceInput])
 
-
+  useEffect(()=>{
+    fetchTrendingList()
+  },[])
 
   return (
     <main>
       <div className='pattern'></div>
       <div className='wrapper'>
         <Header/>
-        <Search 
-        searchTerm={searchTerm} 
-        setSearchTerm={setSearchTerm}
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}
         />
-          
+        {/* conditional rendering of trending page */}
+         {trendingList.length>0 && 
+          <section className='trending'>
+          <h2>Trending Movies</h2>
+          <ul>
+            {trendingList.map((movie,index)=>(
+            <li key={movie.$id}>
+              <p>{index+1}</p>
+              <img src={movie.poster_url} alt={movie.title} />
+            </li>
+          ))}
+          </ul>
+          </section>
+         } 
         <section className='all-movies'>
           <h2 className='mt-[40px]'>All Movies</h2>
 
--- src/appwrite.js
+++ src/appwrite.js
@@ -21,17 +21,38 @@ export const updateSearchCount = async(searchTerm,movie)=>{
 
         if(result.documents.length>0){
             const docs = result.documents[0]
+
             await database.updateDocument(DATABASE_ID,COLLECTION_ID,docs.$id,{count:docs.count+1})
+
         } else {
-            await database.createDocument(DATABASE_ID,COLLECTION_ID,ID.unique(),{
+            if(searchTerm!=''){
+                await database.createDocument(DATABASE_ID,COLLECTION_ID,ID.unique(),{
                 searchTerm,
                 count:1,
                 movie_id:movie.id,
                 poster_url: `https://image.tmbd.org/t/p/w500${movie.poster_path}`
             })
+            }
         }
     } catch (error) {
         console.log(error)
     }
 
+}
+
+
+export const getTrendingMovie = async()=>{
+    try {
+        const result = await database.listDocuments(DATABASE_ID,COLLECTION_ID,[Query.limit(10),
+            Query.orderDesc("count")
+        ])
+
+        // console.log(result.documents)
+        
+     return result.documents
+        
+
+    } catch (error) {
+        console.log(`${error} something wrong has occured`)
+    }
 }
